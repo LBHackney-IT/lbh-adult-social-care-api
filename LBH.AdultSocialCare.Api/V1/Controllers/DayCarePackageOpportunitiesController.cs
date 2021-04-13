481b51f3e8e7272f37e9912b6b7f908d69a45ed8
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-04-13 19:45:26 +0300

    Add swagger xml comments for day care package opportunities

--- LBH.AdultSocialCare.Api/V1/Controllers/DayCarePackageOpportunitiesController.cs
+++ LBH.AdultSocialCare.Api/V1/Controllers/DayCarePackageOpportunitiesController.cs
@@ -1,4 +1,6 @@
+using LBH.AdultSocialCare.Api.V1.Boundary.DayCarePackageBoundary.Response;
 using LBH.AdultSocialCare.Api.V1.Boundary.DayCarePackageOpportunityBoundary.Request;
+using LBH.AdultSocialCare.Api.V1.Boundary.DayCarePackageOpportunityBoundary.Response;
 using LBH.AdultSocialCare.Api.V1.Factories;
 using LBH.AdultSocialCare.Api.V1.UseCase.DayCarePackageOpportunityUseCases.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -6,9 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using LBH.AdultSocialCare.Api.V1.Boundary.DayCarePackageBoundary.Request;
-using LBH.AdultSocialCare.Api.V1.Boundary.DayCarePackageBoundary.Response;
-using LBH.AdultSocialCare.Api.V1.Boundary.DayCarePackageOpportunityBoundary.Response;
 
 namespace LBH.AdultSocialCare.Api.V1.Controllers
 {
@@ -37,6 +36,16 @@ namespace LBH.AdultSocialCare.Api.V1.Controllers
             _updateDayCarePackageOpportunityUseCase = updateDayCarePackageOpportunityUseCase;
         }
 
+        /// <summary>
+        /// Creates the day care package opportunity.
+        /// </summary>
+        /// <param name="dayCarePackageId">The day care package identifier.</param>
+        /// <param name="dayCarePackageOpportunityForCreation">The day care package opportunity for creation.</param>
+        /// <returns>A newly created day care package opportunity</returns>
+        /// <response code="200">Returns ID of the newly created item</response>
+        /// <response code="400">If the item is null</response>
+        /// <response code="404">If the day care package for this opportunity is not found</response>
+        /// <response code="422">If the model is invalid</response>
         [HttpPost]
         [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
@@ -59,17 +68,30 @@ namespace LBH.AdultSocialCare.Api.V1.Controllers
             return Ok(result);
         }
 
-
+        /// <summary>
+        /// Gets a single day care package opportunity.
+        /// </summary>
+        /// <param name="dayCarePackageId">The day care package identifier.</param>
+        /// <param name="dayCarePackageOpportunityId">The day care package opportunity identifier.</param>
+        /// <returns>A day care package opportunity</returns>
+        /// <response code="200">Returns day care package opportunity</response>
+        /// <response code="404">If the day care package or opportunity is not found</response>
         [HttpGet("{dayCarePackageOpportunityId}")]
         [ProducesResponseType(typeof(DayCarePackageResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         [ProducesDefaultResponseType]
-        public async Task<IActionResult> GetSingleDayCarePackage(Guid dayCarePackageId, Guid dayCarePackageOpportunityId)
+        public async Task<IActionResult> GetSingleDayCarePackageOpportunity(Guid dayCarePackageId, Guid dayCarePackageOpportunityId)
         {
             var dayCarePackage = await _getDayCarePackageOpportunityUseCase.Execute(dayCarePackageId, dayCarePackageOpportunityId).ConfigureAwait(false);
             return Ok(dayCarePackage);
         }
 
+        /// <summary>
+        /// Gets the opportunity list for a day care package.
+        /// </summary>
+        /// <param name="dayCarePackageId">The day care package identifier.</param>
+        /// <returns>List of day care package opportunities</returns>
+        /// <response code="200">Returns day care package opportunity list</response>
         [ProducesResponseType(typeof(IEnumerable<DayCarePackageOpportunityResponse>), StatusCodes.Status200OK)]
         [HttpGet("all")]
         public async Task<ActionResult<IEnumerable<DayCarePackageOpportunityResponse>>> GetDayCarePackageOpportunityList(Guid dayCarePackageId)
@@ -77,6 +99,17 @@ namespace LBH.AdultSocialCare.Api.V1.Controllers
             return Ok(await _getDayCarePackageOpportunityListUseCase.Execute(dayCarePackageId).ConfigureAwait(false));
         }
 
+        /// <summary>
+        /// Updates the day care package opportunity.
+        /// </summary>
+        /// <param name="dayCarePackageId">The day care package identifier.</param>
+        /// <param name="dayCarePackageOpportunityId">The day care package opportunity identifier.</param>
+        /// <param name="dayCarePackageOpportunityForUpdate">The day care package opportunity for update.</param>
+        /// <returns>Updated day care package opportunity</returns>
+        /// <response code="200">Returns updated day care package opportunity</response>
+        /// <response code="404">If day care package or opportunity is not found</response>
+        /// <response code="400">If the update item is null</response>
+        /// <response code="422">If model for update is invalid</response>
         [HttpPut("{dayCarePackageOpportunityId}")]
         [ProducesResponseType(typeof(DayCarePackageOpportunityResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
