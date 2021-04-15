11e226cdc4c5dccca350b2e1124f2eff8399dacf
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-04-15 09:16:00 +0100

    Home Care Package approval screen initial

---
+++ LBH.AdultSocialCare.Api/V1/Boundary/Response/HomeCarePackageCostResponse.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LBH.AdultSocialCare.Api.V1.Boundary.Response
+{
+    public class HomeCarePackageCostResponse
+    {
+        /// <summary>
+        /// Gets or sets the Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Home Care Package Id
+        /// </summary>
+        public Guid HomeCarePackageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Service Name
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cost Per Hour
+        /// </summary>
+        public decimal CostPerHour { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Hour Per Week
+        /// </summary>
+        public int HoursPerWeek { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Total Cost
+        /// </summary>
+        public decimal TotalCost { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Creator Id
+        /// </summary>
+        public int CreatorId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Updator Id
+        /// </summary>
+        public int UpdatorId { get; set; }
+    }
+}
---
+++ LBH.AdultSocialCare.Api/V1/Infrastructure/Entities/HomeCarePackageCost.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LBH.AdultSocialCare.Api.V1.Infrastructure.Entities
+{
+    public class HomeCarePackageCost : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the Id
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Home Care Package Id
+        /// </summary>
+        public Guid HomeCarePackageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Service Name
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cost Per Hour
+        /// </summary>
+        public decimal CostPerHour { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Hour Per Week
+        /// </summary>
+        public int HoursPerWeek { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Total Cost
+        /// </summary>
+        public decimal TotalCost { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Creator Id
+        /// </summary>
+        public int CreatorId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Updator Id
+        /// </summary>
+        public int UpdatorId { get; set; }
+    }
+}
