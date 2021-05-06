db7e3c0359ef40978bc14924aa9f993c585676d3
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-05-06 16:53:46 +0100

    code clean up

--- LBH.AdultSocialCare.Api/V1/Gateways/HomeCareBrokerageGateways/HomeCareBrokerageGateway.cs
+++ LBH.AdultSocialCare.Api/V1/Gateways/HomeCareBrokerageGateways/HomeCareBrokerageGateway.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using LBH.AdultSocialCare.Api.V1.Domain.HomeCareBrokerage;
 using LBH.AdultSocialCare.Api.V1.Exceptions;
@@ -6,10 +10,6 @@ using LBH.AdultSocialCare.Api.V1.Infrastructure;
 using LBH.AdultSocialCare.Api.V1.Infrastructure.Entities.HomeCare;
 using LBH.AdultSocialCare.Api.V1.Infrastructure.Entities.HomeCareBrokerage;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
 {
@@ -35,17 +35,14 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
 
             if (itemsToDelete.Count > 0)
             {
-                foreach (var item in itemsToDelete)
-                {
-                    _databaseContext.HomeCarePackageCosts.Remove(item);
-                }
+                foreach (var item in itemsToDelete) _databaseContext.HomeCarePackageCosts.Remove(item);
 
                 await _databaseContext.SaveChangesAsync().ConfigureAwait(false);
             }
 
             foreach (var homeCarePackageCostInput in homeCareBrokerageCreation.HomeCarePackageCost)
             {
-                HomeCarePackageCost homeCarePackageCostToCreate = new HomeCarePackageCost
+                var homeCarePackageCostToCreate = new HomeCarePackageCost
                 {
                     HomeCarePackageId = homeCarePackageId,
                     HomeCareServiceTypeId = homeCarePackageCostInput.HomeCareServiceTypeId,
@@ -79,14 +76,12 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
                 .ConfigureAwait(false);
 
             if (homeCarePackageSlot == null)
-            {
                 throw new ErrorException($"Could not find the Home Care Package Slot {homeCarePackageId}");
-            }
 
             var homeCarePackageCostList = new List<HomeCarePackageCost>();
 
             //Calculate home care services primary carer hours
-            List<HomeCarePackageCost> serviceHoursPerWeek = await _databaseContext.HomeCarePackageSlots
+            var serviceHoursPerWeek = await _databaseContext.HomeCarePackageSlots
                 .Where(item => item.HomeCarePackageId == homeCarePackageId)
                 .GroupBy(l => l.ServiceId)
                 .Select(cl => new HomeCarePackageCost
@@ -100,13 +95,13 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
                 {
                     HomeCarePackageId = homeCarePackageId,
                     HomeCareServiceTypeId = item.HomeCareServiceTypeId,
-                    HoursPerWeek = (double) item.HoursPerWeek / 60
+                    HoursPerWeek = item.HoursPerWeek / 60
                 };
                 homeCarePackageCostList.Add(homeCarePackageCostPrimary);
             }
 
             //Calculate home care services secondary carer hours
-            List<HomeCarePackageCost> secondaryCarerHours = await _databaseContext.HomeCarePackageSlots
+            var secondaryCarerHours = await _databaseContext.HomeCarePackageSlots
                 .Where(item => item.HomeCarePackageId == homeCarePackageId)
                 .GroupBy(l => l.ServiceId)
                 .Select(cl => new HomeCarePackageCost
@@ -121,7 +116,7 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
                     HomeCarePackageId = homeCarePackageId,
                     HomeCareServiceTypeId = item.HomeCareServiceTypeId,
                     IsSecondaryCarer = true,
-                    HoursPerWeek = (double) item.HoursPerWeek / 60
+                    HoursPerWeek = item.HoursPerWeek / 60
                 };
                 homeCarePackageCostList.Add(homeCarePackageCostSecondary);
             }
@@ -133,7 +128,7 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
 
             if (timeSlotList.Count > 0)
             {
-                var callTypes = new Dictionary<int, int>() {{1, 30}, {2, 45}, {3, 60}};
+                var callTypes = new Dictionary<int, int> {{1, 30}, {2, 45}, {3, 60}};
 
                 foreach (var item in callTypes)
                 {
@@ -152,7 +147,7 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.HomeCareBrokerageGateways
                 }
             }
 
-            var homeCareBrokerageDomain = new HomeCareBrokerageDomain()
+            var homeCareBrokerageDomain = new HomeCareBrokerageDomain
             {
                 HomeCarePackage = homeCarePackage.ToDomain(),
                 HomeCarePackageCost = homeCarePackageCostList.ToDomain()
