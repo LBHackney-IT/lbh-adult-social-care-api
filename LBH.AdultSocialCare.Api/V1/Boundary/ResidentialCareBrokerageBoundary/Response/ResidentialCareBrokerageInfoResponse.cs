35483ee2272fabc1dbc6f5369443163077d7ebaf
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-07-01 09:35:18 +0300

    Fix import issues

--- LBH.AdultSocialCare.Api/V1/Boundary/ResidentialCareBrokerageBoundary/Response/ResidentialCareBrokerageInfoResponse.cs
+++ LBH.AdultSocialCare.Api/V1/Boundary/ResidentialCareBrokerageBoundary/Response/ResidentialCareBrokerageInfoResponse.cs
@@ -1,8 +1,5 @@
+using LBH.AdultSocialCare.Api.V1.Boundary.ResidentialCareAdditionalNeedsBoundary.Response;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LBH.AdultSocialCare.Api.V1.Boundary.ResidentialCarePackageBoundary.Response;
 
 namespace LBH.AdultSocialCare.Api.V1.Boundary.ResidentialCareBrokerageBoundary.Response
 {
--- LBH.AdultSocialCare.Api/V1/Infrastructure/Entities/ResidentialCareBrokerage/ResidentialCareBrokerageInfo.cs
+++ LBH.AdultSocialCare.Api/V1/Infrastructure/Entities/ResidentialCareBrokerage/ResidentialCareBrokerageInfo.cs
@@ -1,9 +1,7 @@
+using LBH.AdultSocialCare.Api.V1.Infrastructure.Entities.ResidentialCare;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LBH.AdultSocialCare.Api.V1.Infrastructure.Entities.ResidentialCareBrokerage
 {
