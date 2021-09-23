fd5d7fce1f0fbfb6e3cb6947ead27eeae5238e85
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-09-23 20:10:01 +0300

    Package type in request body

--- LBH.AdultSocialCare.Api/V1/Boundary/ResidentialCare/Request/CarePackageForCreationRequest.cs
+++ LBH.AdultSocialCare.Api/V1/Boundary/ResidentialCare/Request/CarePackageForCreationRequest.cs
@@ -1,3 +1,4 @@
+using LBH.AdultSocialCare.Api.V1.AppConstants.Enums;
 using LBH.AdultSocialCare.Api.V1.Validations;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -12,6 +13,9 @@ namespace LBH.AdultSocialCare.Api.V1.Boundary.ResidentialCare.Request
 
         [Required]
         public string PrimarySupportReason { get; set; }
+
+        [Range(1, 4)]
+        public PackageType PackageType { get; set; }
 
         // Package settings
         [Required]
