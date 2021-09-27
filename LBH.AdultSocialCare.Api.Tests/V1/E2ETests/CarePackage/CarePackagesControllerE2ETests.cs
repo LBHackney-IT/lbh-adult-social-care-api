b508b4555dbd151087fd2657c3cb068d448f0c14
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-09-27 17:14:16 +0300

    E2E test get care package scheduling options

--- LBH.AdultSocialCare.Api.Tests/V1/E2ETests/CarePackage/CarePackagesControllerE2ETests.cs
+++ LBH.AdultSocialCare.Api.Tests/V1/E2ETests/CarePackage/CarePackagesControllerE2ETests.cs
@@ -1,9 +1,11 @@
+using Common.Extensions;
 using FluentAssertions;
 using LBH.AdultSocialCare.Api.Tests.V1.Constants;
 using LBH.AdultSocialCare.Api.Tests.V1.Helper;
 using LBH.AdultSocialCare.Api.V1.AppConstants.Enums;
 using LBH.AdultSocialCare.Api.V1.Boundary.Common.Response;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -37,6 +39,18 @@ namespace LBH.AdultSocialCare.Api.Tests.V1.E2ETests.CarePackage
             response.Content.Id.Should().NotBe(Guid.Empty);
             response.Content.PackageType.Should().Be((int) carePackageCreationRequest.PackageType);
             response.Content.Status.Should().Be((int) PackageStatus.Draft);
+        }
+
+        [Fact]
+        public async Task ShouldReturnPackageSchedulingOptionsList()
+        {
+            var response = await _fixture.RestClient
+                .GetAsync<IEnumerable<CarePackageSchedulingOptionResponse>>("api/v1/care-packages/package-scheduling-options")
+                .ConfigureAwait(false);
+
+            response.Message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            response.Content.Count().Should().Be(Enum.GetNames(typeof(PackageScheduling)).Length);
         }
     }
 }
