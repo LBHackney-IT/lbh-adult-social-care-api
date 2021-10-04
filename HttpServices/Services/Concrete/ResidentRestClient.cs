687843eeb42b6c5c6ea13a2322a41f74136340bd
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-10-04 15:08:24 +0100

    update client service

--- HttpServices/Services/Concrete/ResidentRestClient.cs
+++ HttpServices/Services/Concrete/ResidentRestClient.cs
@@ -18,41 +18,5 @@ namespace HttpServices.Services.Concrete
         {
             _httpClient = httpClient;
         }
-
-        /// <summary>
-        /// Sends a GET method to specified url.
-        /// </summary>
-        public async Task<TResult> GetAsync<TResult>(string url, string errorMessage)
-        {
-            return await SubmitRequest<TResult>(url, null, errorMessage, HttpMethod.Get).ConfigureAwait(false);
-        }
-
-        private async Task<TResult> SubmitRequest<TResult>(string url, object payload, string errorMessage, HttpMethod method)
-        {
-            var httpRequestMessage = new HttpRequestMessage
-            {
-                Method = method,
-                RequestUri = new Uri($"{_httpClient.BaseAddress}{url}")
-            };
-
-            if (payload != null)
-            {
-                var jsonBody = JsonConvert.SerializeObject(payload);
-                httpRequestMessage.Content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-            }
-
-            var httpResponse = await _httpClient.SendAsync(httpRequestMessage).ConfigureAwait(false);
-
-            if (!httpResponse.IsSuccessStatusCode)
-            {
-                await httpResponse.ThrowResponseExceptionAsync(errorMessage).ConfigureAwait(false);
-            }
-
-            if (httpResponse.Content == null ||
-                httpResponse.Content.Headers.ContentType?.MediaType != "application/json") return default;
-
-            var content = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<TResult>(content);
-        }
     }
 }
