496caeed49ea627991929319443a194597f8387d
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-10-06 21:00:20 +0300

    Add navigation in identity tables

---
+++ LBH.AdultSocialCare.Api/V1/Infrastructure/Entities/AppUserRole.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace LBH.AdultSocialCare.Api.V1.Infrastructure.Entities
+{
+    public class AppUserRole : IdentityUserRole<Guid>
+    {
+        public virtual Role Role { get; set; }
+        public virtual User User { get; set; }
+    }
+}
