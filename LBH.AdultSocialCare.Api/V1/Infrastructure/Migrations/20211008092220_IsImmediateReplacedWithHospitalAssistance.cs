74ba3e22f2b6b78235e449207b8bd6b1b077d0cc
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2021-10-08 12:24:20 +0300

    migration for HospitalAvoidance setting

---
+++ LBH.AdultSocialCare.Api/V1/Infrastructure/Migrations/20211008092220_IsImmediateReplacedWithHospitalAssistance.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LBH.AdultSocialCare.Api.V1.Infrastructure.Migrations
+{
+    public partial class IsImmediateReplacedWithHospitalAssistance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsImmediate",
+                table: "CarePackageSettings");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HospitalAvoidance",
+                table: "CarePackageSettings",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HospitalAvoidance",
+                table: "CarePackageSettings");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsImmediate",
+                table: "CarePackageSettings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
