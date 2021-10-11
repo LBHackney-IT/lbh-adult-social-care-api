45cb87d22c76e402baf400b6ff92f23c1b6e66e3
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-10-11 12:51:22 +0300

    Seed more suppliers

--- LBH.AdultSocialCare.Api/V1/Infrastructure/SeedConfiguration/SupplierSeed.cs
+++ LBH.AdultSocialCare.Api/V1/Infrastructure/SeedConfiguration/SupplierSeed.cs
@@ -15,8 +15,8 @@ namespace LBH.AdultSocialCare.Api.V1.Infrastructure.SeedConfiguration
             {
                 Id = 1,
                 PackageTypeId = 1,
-                SupplierName = "ABC Limited",
-                Address = "15 Atherden Rd, Lower Clapton, London E5 0QP",
+                SupplierName = "Abbeleigh House",
+                Address = "Abbeleigh House",
                 IsSupplierInternal = true,
                 HasSupplierFrameworkContractedRates = true,
                 FundedNursingCareCollector = null,
@@ -26,8 +26,104 @@ namespace LBH.AdultSocialCare.Api.V1.Infrastructure.SeedConfiguration
             }, new Supplier
             {
                 Id = 2,
-                SupplierName = "XYZ Ltd",
-                Address = "54 Crown Street, Belgravia, London WC1E 9YP",
+                SupplierName = "Abbey Care Complex",
+                Address = "Abbey Care Complex",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 3,
+                SupplierName = "Acacia Lodge",
+                Address = "Acacia Lodge [Cedar Site 0]",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 4,
+                SupplierName = "Hc-One Limited",
+                Address = "Hc-One Limited",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 5,
+                SupplierName = "Acorn Lodge",
+                Address = "Acorn Lodge",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 6,
+                SupplierName = "Albany Nursing Home",
+                Address = "Albany Nursing Home",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 7,
+                SupplierName = "Manor Farm Care Home",
+                Address = "Manor Farm Care Home",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 8,
+                SupplierName = "Four Seasons Health Care",
+                Address = "Four Seasons Health Care [Cedar Site 8] Lingfield Point",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 9,
+                SupplierName = "The Hornchurch Care Home",
+                Address = "The Hornchurch Care Home",
+                PackageTypeId = 1,
+                IsSupplierInternal = true,
+                HasSupplierFrameworkContractedRates = true,
+                FundedNursingCareCollector = null,
+                CreatorId = new Guid("aee45700-af9b-4ab5-bb43-535adbdcfb84"),
+                DateCreated = dateTimeOffset,
+                DateUpdated = dateTimeOffset
+            }, new Supplier
+            {
+                Id = 10,
+                SupplierName = "Bupa Care Homes",
+                Address = "Bupa Care Homes [Cedar Site 10] Wynne Road",
                 PackageTypeId = 1,
                 IsSupplierInternal = true,
                 HasSupplierFrameworkContractedRates = true,
