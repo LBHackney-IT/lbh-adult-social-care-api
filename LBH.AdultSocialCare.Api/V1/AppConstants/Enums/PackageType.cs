e35b5906b75efdfb5fe75c69b6fbe1e7c841a05b
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2021-10-11 13:19:57 +0300

    Commented out unsupported package types

--- LBH.AdultSocialCare.Api/V1/AppConstants/Enums/PackageType.cs
+++ LBH.AdultSocialCare.Api/V1/AppConstants/Enums/PackageType.cs
@@ -4,16 +4,16 @@ namespace LBH.AdultSocialCare.Api.V1.AppConstants.Enums
 {
     public enum PackageType
     {
-        [Display(Name = "Home Care Package")]
-        HomeCare = 1,
+        // [Display(Name = "Home Care Package")]
+        // HomeCare = 1,
 
         [Display(Name = "Residential Care Package")]
         ResidentialCare = 2,
 
-        [Display(Name = "Day Care Package")]
-        DayCare = 3,
+        // [Display(Name = "Day Care Package")]
+        // DayCare = 3,
 
         [Display(Name = "Nursing Care Package")]
-        NursingCare = 4,
+        NursingCare = 4
     }
 }
