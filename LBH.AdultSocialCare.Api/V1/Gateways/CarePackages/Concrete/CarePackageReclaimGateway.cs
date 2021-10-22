c2113a6db5b18b55b880110456db0affe6223201
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-10-22 15:51:34 +0100

    fix provisional care charge start date

--- LBH.AdultSocialCare.Api/V1/Gateways/CarePackages/Concrete/CarePackageReclaimGateway.cs
+++ LBH.AdultSocialCare.Api/V1/Gateways/CarePackages/Concrete/CarePackageReclaimGateway.cs
@@ -29,11 +29,18 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.CarePackages.Concrete
         public async Task<CarePackageReclaimDomain> CreateAsync(CarePackageReclaim carePackageReclaim)
         {
             var carePackage = await _dbContext.CarePackages
+                .Include(cp => cp.Details)
                 .FirstOrDefaultAsync(item => item.Id == carePackageReclaim.CarePackageId);
 
             if (carePackage == null)
             {
                 throw new EntityNotFoundException($"Unable to locate care package for {carePackageReclaim.CarePackageId}");
+            }
+
+            if (carePackageReclaim.SubType == ReclaimSubType.CareChargeProvisional)
+            {
+                var coreCostDetail = carePackage.Details.FirstOrDefault(d => d.Type is PackageDetailType.CoreCost);
+                if (coreCostDetail != null) carePackageReclaim.StartDate = coreCostDetail.StartDate;
             }
 
             var entry = await _dbContext.CarePackageReclaims.AddAsync(carePackageReclaim);
