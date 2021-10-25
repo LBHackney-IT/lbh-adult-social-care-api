a6277333134aa884bb463e4c4b7edbfdcf711276
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-10-25 16:42:27 +0100

    set not approved status of package

--- LBH.AdultSocialCare.Api.Tests/V1/E2ETests/CarePackages/CarePackagesControllerE2ETests.cs
+++ LBH.AdultSocialCare.Api.Tests/V1/E2ETests/CarePackages/CarePackagesControllerE2ETests.cs
@@ -248,7 +248,7 @@ namespace LBH.AdultSocialCare.Api.Tests.V1.E2ETests.CarePackages
         [InlineData("end", PackageStatus.Ended, HistoryStatus.BrokeredEnded)]
         [InlineData("cancel", PackageStatus.Cancelled, HistoryStatus.Cancelled)]
         [InlineData("approve", PackageStatus.Approved, HistoryStatus.PackageApproved)]
-        [InlineData("decline", PackageStatus.Rejected, HistoryStatus.Rejected)]
+        [InlineData("decline", PackageStatus.NotApproved, HistoryStatus.Declined)]
         public async Task ShouldEndPackage(string endpoint, PackageStatus packageStatus, HistoryStatus historyStatus)
         {
             var package = _generator.CreateCarePackage();
