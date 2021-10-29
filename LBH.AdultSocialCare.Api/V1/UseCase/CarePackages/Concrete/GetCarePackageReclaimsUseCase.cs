0005bdaed8c3a698fe602bbcae99477d8a44dbc8
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-10-29 10:30:32 +0100

    Return 404 for CareCharges, if the package not found.

--- LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/GetCarePackageReclaimsUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/GetCarePackageReclaimsUseCase.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Common.Extensions;
 using LBH.AdultSocialCare.Api.V1.AppConstants.Enums;
 using LBH.AdultSocialCare.Api.V1.Boundary.CarePackages.Response;
 using LBH.AdultSocialCare.Api.V1.Domain.CarePackages;
 using LBH.AdultSocialCare.Api.V1.Factories;
 using LBH.AdultSocialCare.Api.V1.Gateways.CarePackages.Interfaces;
+using LBH.AdultSocialCare.Api.V1.Gateways.Enums;
 using LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Interfaces;
 
 namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
@@ -13,10 +15,12 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
     public class GetCarePackageReclaimsUseCase : IGetCarePackageReclaimsUseCase
     {
         private readonly ICarePackageReclaimGateway _carePackageReclaimGateway;
+        private readonly ICarePackageGateway _carePackageGateway;
 
-        public GetCarePackageReclaimsUseCase(ICarePackageReclaimGateway carePackageReclaimGateway)
+        public GetCarePackageReclaimsUseCase(ICarePackageReclaimGateway carePackageReclaimGateway, ICarePackageGateway carePackageGateway)
         {
             _carePackageReclaimGateway = carePackageReclaimGateway;
+            _carePackageGateway = carePackageGateway;
         }
 
         public async Task<CarePackageReclaimResponse> GetCarePackageReclaim(Guid carePackageId, ReclaimType reclaimType)
@@ -27,6 +31,9 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
 
         public async Task<IEnumerable<CarePackageReclaimDomain>> GetListAsync(Guid carePackageId, ReclaimType? reclaimType, ReclaimSubType? reclaimSubType)
         {
+            await _carePackageGateway.GetPackageAsync(carePackageId)
+                .EnsureExistsAsync($"Care package {carePackageId} not found");
+
             var result = await _carePackageReclaimGateway.GetListAsync(carePackageId, reclaimType, reclaimSubType);
             return result.ToDomain();
         }
--- LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/GetSinglePackageCareChargeUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/GetSinglePackageCareChargeUseCase.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using Common.Extensions;
 using LBH.AdultSocialCare.Api.V1.Boundary.CarePackages.Response;
 using LBH.AdultSocialCare.Api.V1.Factories;
 using LBH.AdultSocialCare.Api.V1.Gateways.CarePackages.Interfaces;
+using LBH.AdultSocialCare.Api.V1.Gateways.Enums;
 using LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Interfaces;
 using LBH.AdultSocialCare.Api.V1.UseCase.Common.Interfaces;
 
@@ -11,14 +13,19 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
     public class GetSinglePackageCareChargeUseCase : IGetSinglePackageCareChargeUseCase
     {
         private readonly ICarePackageReclaimGateway _carePackageReclaimGateway;
+        private readonly ICarePackageGateway _carePackageGateway;
 
-        public GetSinglePackageCareChargeUseCase(ICarePackageReclaimGateway carePackageReclaimGateway)
+        public GetSinglePackageCareChargeUseCase(ICarePackageReclaimGateway carePackageReclaimGateway, ICarePackageGateway carePackageGateway)
         {
             _carePackageReclaimGateway = carePackageReclaimGateway;
+            _carePackageGateway = carePackageGateway;
         }
 
         public async Task<SinglePackageCareChargeResponse> GetSinglePackageCareCharge(Guid packageId)
         {
+            await _carePackageGateway.GetPackageAsync(packageId)
+                .EnsureExistsAsync($"Care package {packageId} not found");
+
             var singlePackageCareCharge = await _carePackageReclaimGateway.GetSinglePackageCareCharge(packageId).ConfigureAwait(false);
             return singlePackageCareCharge.ToResponse();
         }
