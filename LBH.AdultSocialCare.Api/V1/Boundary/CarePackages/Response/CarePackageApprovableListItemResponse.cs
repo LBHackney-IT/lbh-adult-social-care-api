62ab821964d8af42cd5170b3b121c66d718e34b9
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2021-10-29 18:52:40 +0300

    approvals response: added package type name

--- LBH.AdultSocialCare.Api/V1/Boundary/CarePackages/Response/CarePackageApprovableListItemResponse.cs
+++ LBH.AdultSocialCare.Api/V1/Boundary/CarePackages/Response/CarePackageApprovableListItemResponse.cs
@@ -1,4 +1,5 @@
 using System;
+using Common.Extensions;
 using LBH.AdultSocialCare.Api.V1.AppConstants.Enums;
 using LBH.AdultSocialCare.Api.V1.Boundary.Common.Response;
 
@@ -10,8 +11,10 @@ namespace LBH.AdultSocialCare.Api.V1.Boundary.CarePackages.Response
         public UsersMinimalResponse Approver { get; set; }
 
         public Guid Id { get; set; }
-        public PackageType PackageType { get; set; }
         public PackageStatus Status { get; set; }
+
+        public PackageType PackageType { get; set; }
+        public string PackageTypeName => PackageType.GetDisplayName();
 
         public DateTimeOffset? DateAssigned { get; set; }
     }
