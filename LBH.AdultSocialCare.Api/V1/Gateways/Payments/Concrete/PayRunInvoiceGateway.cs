fd56a7a8faabc2b6d5b729db9da1fa7d7129b61a
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-11-08 17:23:48 +0300

    Get pay run details

--- LBH.AdultSocialCare.Api/V1/Gateways/Payments/Concrete/PayRunInvoiceGateway.cs
+++ LBH.AdultSocialCare.Api/V1/Gateways/Payments/Concrete/PayRunInvoiceGateway.cs
@@ -1,3 +1,4 @@
+using LBH.AdultSocialCare.Api.V1.Domain.Payments;
 using LBH.AdultSocialCare.Api.V1.Extensions;
 using LBH.AdultSocialCare.Api.V1.Gateways.Enums;
 using LBH.AdultSocialCare.Api.V1.Gateways.Payments.Interfaces;
@@ -35,6 +36,46 @@ namespace LBH.AdultSocialCare.Api.V1.Gateways.Payments.Concrete
 
             var invoiceCount = await query.CountAsync();
             return PagedList<PayrunInvoice>.ToPagedList(payRunInvoices, invoiceCount, parameters.PageNumber,
+                parameters.PageSize);
+        }
+
+        public async Task<PagedList<PayRunInvoiceDomain>> GetPayRunInvoicesSummaryAsync(Guid payRunId,
+            PayRunDetailsQueryParameters parameters)
+        {
+            var query = _dbContext.PayrunInvoices.Where(p => p.PayrunId == payRunId)
+                .FilterPayRunInvoices(parameters)
+                .TrackChanges(false);
+
+            var payRunInvoices = await query.Select(payRunInvoice => new PayRunInvoiceDomain
+            {
+                InvoiceId = payRunInvoice.InvoiceId,
+                CarePackageId = payRunInvoice.Invoice.PackageId,
+                ServiceUserId = payRunInvoice.Invoice.ServiceUserId,
+                ServiceUserName =
+                    $"{payRunInvoice.Invoice.ServiceUser.FirstName} {payRunInvoice.Invoice.ServiceUser.MiddleName ?? string.Empty} {payRunInvoice.Invoice.ServiceUser.LastName}",
+                SupplierId = payRunInvoice.Invoice.SupplierId,
+                SupplierName = payRunInvoice.Invoice.Supplier.SupplierName,
+                InvoiceNumber = payRunInvoice.Invoice.Number,
+                PackageType = payRunInvoice.Invoice.Package.PackageType,
+                InvoiceStatus = payRunInvoice.InvoiceStatus,
+                AssignedBrokerName = payRunInvoice.Invoice.Package.Broker.Name,
+                InvoiceItems = payRunInvoice.Invoice.Items.Select(ii => new PayRunInvoiceItemDomain
+                {
+                    Id = ii.Id,
+                    Name = ii.Name,
+                    FromDate = ii.FromDate,
+                    ToDate = ii.ToDate,
+                    Cost = ii.WeeklyCost,
+                    Quantity = ii.Quantity,
+                    TotalCost = ii.TotalCost,
+                    IsReclaim = ii.IsReclaim ?? false,
+                    ClaimCollector = ii.ClaimCollector,
+                    PriceEffect = ii.PriceEffect
+                })
+            }).ToListAsync();
+
+            var invoiceCount = await query.CountAsync();
+            return PagedList<PayRunInvoiceDomain>.ToPagedList(payRunInvoices, invoiceCount, parameters.PageNumber,
                 parameters.PageSize);
         }
 
--- LBH.AdultSocialCare.Api/V1/UseCase/Payments/Concrete/GetPayRunDetailsUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/Payments/Concrete/GetPayRunDetailsUseCase.cs
@@ -3,21 +3,52 @@ using HttpServices.Models.Features;
 using LBH.AdultSocialCare.Api.V1.AppConstants.Enums;
 using LBH.AdultSocialCare.Api.V1.Boundary.Common.Response;
 using LBH.AdultSocialCare.Api.V1.Boundary.PayRuns.Response;
+using LBH.AdultSocialCare.Api.V1.Gateways.Enums;
+using LBH.AdultSocialCare.Api.V1.Gateways.Payments.Interfaces;
+using LBH.AdultSocialCare.Api.V1.Infrastructure.Entities.Payments;
 using LBH.AdultSocialCare.Api.V1.Infrastructure.RequestFeatures.Parameters;
 using LBH.AdultSocialCare.Api.V1.UseCase.Payments.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using LBH.AdultSocialCare.Api.V1.Domain.Payments;
 
 namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
 {
     public class GetPayRunDetailsUseCase : IGetPayRunDetailsUseCase
     {
+        private readonly IPayRunGateway _payRunGateway;
+        private readonly IPayRunInvoiceGateway _payRunInvoiceGateway;
+
+        public GetPayRunDetailsUseCase(IPayRunGateway payRunGateway, IPayRunInvoiceGateway payRunInvoiceGateway)
+        {
+            _payRunGateway = payRunGateway;
+            _payRunInvoiceGateway = payRunInvoiceGateway;
+        }
+
         public async Task<PayRunDetailsViewResponse> ExecuteAsync(Guid payrunId, PayRunDetailsQueryParameters parameters)
         {
-            var result = new PayRunDetailsViewResponse
+            // Get pay run
+            var payRun = await _payRunGateway.GetPayRunAsync(payrunId);
+            if (payRun == null)
+            {
+                return GenerateDummyPayRunData(payrunId);
+            }
+
+            // Get pay run items
+            var payRunInvoices =
+                await _payRunInvoiceGateway.GetPayRunInvoicesSummaryAsync(payrunId, parameters);
+            var result = CreateResponse(payRun, payRunInvoices, payRunInvoices.PagingMetaData);
+            return result;
+        }
+
+        private static PayRunDetailsViewResponse GenerateDummyPayRunData(Guid payrunId)
+        {
+            return new PayRunDetailsViewResponse
             {
                 PayRunId = payrunId,
+                PayRunNumber = payrunId.ToString().Substring(0, 6),
                 DateCreated = DateTimeOffset.Now,
                 StartDate = DateTimeOffset.Now.AddDays(-14),
                 EndDate = DateTimeOffset.Now,
@@ -29,7 +60,6 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
                     {
                         new PayRunInvoiceResponse
                         {
-                            Id = Guid.NewGuid(),
                             InvoiceId = Guid.NewGuid(),
                             CarePackageId = Guid.NewGuid(),
                             ServiceUserId = Guid.NewGuid(),
@@ -109,7 +139,6 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
                         },
                         new PayRunInvoiceResponse
                         {
-                            Id = Guid.NewGuid(),
                             InvoiceId = Guid.NewGuid(),
                             CarePackageId = Guid.NewGuid(),
                             ServiceUserId = Guid.NewGuid(),
@@ -189,14 +218,13 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
                         },
                         new PayRunInvoiceResponse
                         {
-                            Id = Guid.NewGuid(),
                             InvoiceId = Guid.NewGuid(),
                             CarePackageId = Guid.NewGuid(),
                             ServiceUserId = Guid.NewGuid(),
                             ServiceUserName = "Martin Ayeni",
                             SupplierId = 12,
                             SupplierName = "Barchester Healthcare Homes Ltd",
-                            InvoiceNumber = "INV 11",
+                            InvoiceNumber = "INV 12",
                             PackageTypeId = (int) PackageType.ResidentialCare,
                             PackageType = PackageType.ResidentialCare.GetDisplayName(),
                             GrossTotal = 800,
@@ -270,8 +298,87 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
                     }
                 }
             };
+        }
 
-            return await Task.FromResult(result);
+        private static PayRunDetailsViewResponse CreateResponse(Payrun payrun, IEnumerable<PayRunInvoiceDomain> payRunInvoices, PagingMetaData pagingMetaData)
+        {
+            var result = new PayRunDetailsViewResponse
+            {
+                PayRunId = payrun.Id,
+                DateCreated = payrun.DateCreated,
+                StartDate = payrun.StartDate,
+                EndDate = payrun.EndDate
+            };
+
+            //
+            var invoices = new List<PayRunInvoiceResponse>();
+
+            foreach (var invoice in payRunInvoices)
+            {
+                var (grossTotal, netTotal) = CalculateTotals(invoice.InvoiceItems);
+                var invoiceRes = new PayRunInvoiceResponse
+                {
+                    InvoiceId = invoice.InvoiceId,
+                    CarePackageId = invoice.CarePackageId,
+                    ServiceUserId = invoice.ServiceUserId,
+                    ServiceUserName = invoice.ServiceUserName,
+                    SupplierId = invoice.SupplierId,
+                    SupplierName = invoice.SupplierName,
+                    InvoiceNumber = invoice.InvoiceNumber,
+                    PackageTypeId = (int) invoice.PackageType,
+                    PackageType = invoice.PackageType.GetDisplayName(),
+                    GrossTotal = grossTotal,
+                    NetTotal = netTotal,
+                    InvoiceStatus = invoice.InvoiceStatus,
+                    AssignedBrokerName = invoice.AssignedBrokerName,
+                    InvoiceItems = invoice.InvoiceItems.Select(ii => new PayRunInvoiceItemResponse
+                    {
+                        Id = ii.Id,
+                        Name = ii.Name,
+                        FromDate = ii.FromDate,
+                        ToDate = ii.ToDate,
+                        Cost = ii.Cost,
+                        Days = (ii.ToDate - ii.FromDate).Days,
+                        Quantity = ii.Quantity,
+                        Period = $"{(ii.ToDate - ii.FromDate).Days} days",
+                        TotalCost = ii.TotalCost,
+                        IsReclaim = ii.IsReclaim,
+                        ClaimCollector = ii.ClaimCollector,
+                        ClaimCollectorName = ii.ClaimCollector.GetDisplayName()
+                    })
+                };
+                invoices.Add(invoiceRes);
+            }
+
+            result.PayRunItems = new PagedResponse<PayRunInvoiceResponse>
+            {
+                PagingMetaData = pagingMetaData,
+                Data = invoices
+            };
+
+            return result;
+        }
+
+        private static (decimal, decimal) CalculateTotals(IEnumerable<PayRunInvoiceItemDomain> invoiceItems)
+        {
+            var grossTotal = 0M;
+            var netTotal = 0M;
+
+            foreach (var invoiceItem in invoiceItems)
+            {
+                if (invoiceItem.IsReclaim == false && invoiceItem.PriceEffect == PriceEffect.Add)
+                {
+                    grossTotal += invoiceItem.TotalCost;
+                    netTotal += invoiceItem.TotalCost;
+                }
+
+                if (invoiceItem.IsReclaim && invoiceItem.ClaimCollector == ClaimCollector.Supplier && invoiceItem.PriceEffect == PriceEffect.Subtract)
+                {
+                    netTotal -= invoiceItem.TotalCost;
+                }
+            }
+
+            return (grossTotal, netTotal);
         }
     }
 }
