47da681b10ed75da592ede31665a640c74f8bc00
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-11-11 18:15:59 +0300

    Hold invoice data type fix

--- LBH.AdultSocialCare.Api/V1/Boundary/Payments/Request/HeldInvoiceCreationRequest.cs
+++ LBH.AdultSocialCare.Api/V1/Boundary/Payments/Request/HeldInvoiceCreationRequest.cs
@@ -1,7 +1,5 @@
 using LBH.AdultSocialCare.Api.Attributes;
 using LBH.AdultSocialCare.Api.V1.Domain.Payments;
-using LBH.AdultSocialCare.Api.V1.Validations;
-using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace LBH.AdultSocialCare.Api.V1.Boundary.Payments.Request
@@ -9,7 +7,7 @@ namespace LBH.AdultSocialCare.Api.V1.Boundary.Payments.Request
     [GenerateMappingFor(typeof(HeldInvoiceCreationDomain))]
     public class HeldInvoiceCreationRequest
     {
-        [Required, GuidNotEmpty] public Guid ActionRequiredFromId { get; set; }
+        [Required, Range(1, int.MaxValue)] public int ActionRequiredFromId { get; set; }
         [Required] public string ReasonForHolding { get; set; }
     }
 }
