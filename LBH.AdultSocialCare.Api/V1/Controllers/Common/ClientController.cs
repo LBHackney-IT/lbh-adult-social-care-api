06d77f30a995a217ed91728fe7706de860ac7f53
mdrizy96 <duncanokeno@gmail.com>
2021-11-20 13:46:12 +0300

    Add authorize roles on controllers

--- LBH.AdultSocialCare.Api/V1/Controllers/Common/ClientController.cs
+++ LBH.AdultSocialCare.Api/V1/Controllers/Common/ClientController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using LBH.AdultSocialCare.Data.Constants.Enums;
 using LBH.AdultSocialCare.Data.RequestFeatures.Parameters;
 
 namespace LBH.AdultSocialCare.Api.V1.Controllers.Common
@@ -44,6 +45,7 @@ namespace LBH.AdultSocialCare.Api.V1.Controllers.Common
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [ProducesDefaultResponseType]
         [HttpPost]
+        [AuthorizeRoles(RolesEnum.Broker)]
         public async Task<ActionResult<ServiceUserResponse>> Create(ServiceUserRequest serviceUserRequest)
         {
             try
@@ -110,6 +112,7 @@ namespace LBH.AdultSocialCare.Api.V1.Controllers.Common
         [ProducesDefaultResponseType]
         [HttpDelete]
         [Route("{clientId}")]
+        [AuthorizeRoles(RolesEnum.Broker)]
         public async Task<ActionResult<bool>> Delete(Guid clientId)
         {
             try
