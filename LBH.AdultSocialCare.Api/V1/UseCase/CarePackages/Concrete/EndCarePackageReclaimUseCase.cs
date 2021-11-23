44da6c95d2aa951b01058fe99c3b03755baa347a
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-11-23 10:11:05 +0000

    Check overlap when the user end or cancel package

--- LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/EndCarePackageReclaimUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/EndCarePackageReclaimUseCase.cs
@@ -5,8 +5,13 @@ using LBH.AdultSocialCare.Api.V1.Gateways;
 using LBH.AdultSocialCare.Api.V1.Gateways.CarePackages.Interfaces;
 using LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Interfaces;
 using System;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Common.Exceptions.CustomExceptions;
+using LBH.AdultSocialCare.Api.V1.Extensions;
 using LBH.AdultSocialCare.Api.V1.Factories;
+using LBH.AdultSocialCare.Api.V1.Gateways.Enums;
 using LBH.AdultSocialCare.Data.Constants.Enums;
 using LBH.AdultSocialCare.Data.Entities.CarePackages;
 
@@ -17,12 +22,15 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
         private readonly ICarePackageReclaimGateway _gateway;
         private readonly IDatabaseManager _dbManager;
         private readonly ICarePackageHistoryGateway _carePackageHistoryGateway;
+        private readonly ICarePackageGateway _carePackageGateway;
 
-        public EndCarePackageReclaimUseCase(ICarePackageReclaimGateway gateway, IDatabaseManager dbManager, ICarePackageHistoryGateway carePackageHistoryGateway)
+        public EndCarePackageReclaimUseCase(ICarePackageReclaimGateway gateway, IDatabaseManager dbManager
+            , ICarePackageHistoryGateway carePackageHistoryGateway, ICarePackageGateway carePackageGateway)
         {
             _gateway = gateway;
             _dbManager = dbManager;
             _carePackageHistoryGateway = carePackageHistoryGateway;
+            _carePackageGateway = carePackageGateway;
         }
 
         public async Task<CarePackageReclaimDomain> ExecuteAsync(Guid reclaimId, CarePackageReclaimEndRequest request)
@@ -33,6 +41,11 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
 
             reclaim.Status = ReclaimStatus.Ended;
             reclaim.EndDate = request.EndDate?.Date;
+
+            var carePackage = await _carePackageGateway
+                .GetPackageAsync(reclaim.CarePackageId, PackageFields.Reclaims, true);
+
+            await EndCareChargeWithoutPropertyThirteenPlusWeeks(reclaim, carePackage);
 
             var history = new CarePackageHistory
             {
@@ -45,6 +58,29 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
             await _dbManager.SaveAsync();
 
             return reclaim.ToDomain();
+        }
+
+        private async Task EndCareChargeWithoutPropertyThirteenPlusWeeks(CarePackageReclaim carePackageReclaim, CarePackage carePackage)
+        {
+            // check existing reclaims in status Active and Pending
+            var existingReclaims = carePackage.Reclaims
+                .Where(r => r.Status.In(ReclaimStatus.Active, ReclaimStatus.Pending));
+
+            // if user end CareChargeWithoutPropertyOneToTwelveWeeks end CareChargeWithoutPropertyThirteenPlusWeeks as well
+            if (carePackageReclaim.SubType == ReclaimSubType.CareChargeWithoutPropertyOneToTwelveWeeks)
+            {
+                var existingCareChargeWithoutPropertyThirteenPlusWeeks =
+                    existingReclaims.FirstOrDefault(r => r.SubType == ReclaimSubType.CareChargeWithoutPropertyThirteenPlusWeeks);
+
+                if (existingCareChargeWithoutPropertyThirteenPlusWeeks != null)
+                {
+                    var reclaim = await _gateway
+                        .GetAsync(existingCareChargeWithoutPropertyThirteenPlusWeeks.Id);
+
+                    reclaim.Status = ReclaimStatus.Ended;
+                    reclaim.EndDate = carePackageReclaim.EndDate?.Date;
+                }
+            }
         }
     }
 }
