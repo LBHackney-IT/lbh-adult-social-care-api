8b0b417d3786ae8e4713ec6898cb1a3dc5763c74
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-11-25 23:31:32 +0300

    Firstname lastname filter update

--- LBH.AdultSocialCare.Data/Extensions/GatewayQueryExtensions.cs
+++ LBH.AdultSocialCare.Data/Extensions/GatewayQueryExtensions.cs
@@ -96,7 +96,8 @@ namespace LBH.AdultSocialCare.Data.Extensions
         public static IQueryable<PayrunInvoice> FilterPayRunInvoices(this IQueryable<PayrunInvoice> invoices, PayRunDetailsQueryParameters parameters) =>
             invoices.Where(invoice => (parameters.PackageType == null || invoice.Invoice.Package.PackageType == parameters.PackageType)
                                       && (string.IsNullOrEmpty(parameters.SearchTerm)
-                                          || EF.Functions.ILike($"{invoice.Invoice.ServiceUser.FirstName} {invoice.Invoice.ServiceUser.LastName}", $"%{parameters.SearchTerm}%")
+                                          || (invoice.Invoice.ServiceUser.FirstName.ToLower() + " " +
+                                              invoice.Invoice.ServiceUser.LastName.ToLower()).Contains(parameters.SearchTerm.ToLower())
                                           || invoice.InvoiceId.ToString().Contains(parameters.SearchTerm.ToLower())
                                           || invoice.Invoice.Supplier.SupplierName.ToLower().Contains(parameters.SearchTerm.ToLower()))
                                       && (parameters.InvoiceStatus == null || invoice.InvoiceStatus == parameters.InvoiceStatus)
