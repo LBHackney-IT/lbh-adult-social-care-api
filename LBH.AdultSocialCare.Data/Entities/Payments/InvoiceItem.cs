987c02b791bc27ad0bef0f631d5afc0d32fddd65
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-11-25 17:10:32 +0300

    Decimal precision on amount and quantity

--- LBH.AdultSocialCare.Data/Entities/Payments/InvoiceItem.cs
+++ LBH.AdultSocialCare.Data/Entities/Payments/InvoiceItem.cs
@@ -1,9 +1,9 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using LBH.AdultSocialCare.Data.Constants.Enums;
 using LBH.AdultSocialCare.Data.Entities.CarePackages;
 using LBH.AdultSocialCare.Data.Entities.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LBH.AdultSocialCare.Data.Entities.Payments
 {
@@ -18,10 +18,9 @@ namespace LBH.AdultSocialCare.Data.Entities.Payments
         public Guid? CarePackageDetailId { get; set; }
         public Guid? CarePackageReclaimId { get; set; }
 
-        public decimal WeeklyCost { get; set; }
-        public decimal TotalCost { get; set; }
-
-        public decimal Quantity { get; set; }
+        [Column(TypeName = "decimal(13, 2)")] public decimal WeeklyCost { get; set; }
+        [Column(TypeName = "decimal(13, 2)")] public decimal TotalCost { get; set; }
+        [Column(TypeName = "decimal(7, 2)")] public decimal Quantity { get; set; }
 
         public DateTimeOffset FromDate { get; set; }
         public DateTimeOffset ToDate { get; set; }
@@ -29,8 +28,7 @@ namespace LBH.AdultSocialCare.Data.Entities.Payments
         public ClaimCollector? ClaimCollector { get; set; }
         public PriceEffect PriceEffect { get; set; }
 
-        [ForeignKey(nameof(InvoiceId))]
-        public Invoice Invoice { get; set; }
+        [ForeignKey(nameof(InvoiceId))] public Invoice Invoice { get; set; }
 
         [ForeignKey(nameof(CarePackageDetailId))]
         public CarePackageDetail CarePackageDetail { get; set; }
