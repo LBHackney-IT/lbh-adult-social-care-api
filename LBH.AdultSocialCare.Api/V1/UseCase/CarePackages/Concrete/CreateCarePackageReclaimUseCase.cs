0095932de1de08cfec2dc4b13338a55c4f13a7ba
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-11-26 11:36:48 +0000

    comment validation care charge

--- LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/CreateCarePackageReclaimUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/CreateCarePackageReclaimUseCase.cs
@@ -58,7 +58,7 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
                 newReclaim = TryHandleProvisionalCareCharge(reclaimCreationDomain, coreCostDetail, carePackage);
             }
 
-            ValidateCareChargeAsync(reclaimCreationDomain, coreCostDetail, carePackage);
+            //ValidateCareChargeAsync(reclaimCreationDomain, coreCostDetail, carePackage);
 
             if (newReclaim is null)
             {
