5516782825c19edbb945a520f0847eb14cee0edb
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-11-28 22:04:36 +0000

    update HasAssessmentBeenCarried on FNC

--- LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/UpdateCarePackageReclaimUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/CarePackages/Concrete/UpdateCarePackageReclaimUseCase.cs
@@ -46,9 +46,9 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.CarePackages.Concrete
         public async Task UpdateAsync(CarePackageReclaimUpdateDomain carePackageReclaimUpdateDomain)
         {
             var carePackageReclaim = await _carePackageReclaimGateway.GetAsync(carePackageReclaimUpdateDomain.Id)
-                .EnsureExistsAsync($"Care package reclaim with id {carePackageReclaimUpdateDomain.Id} not found"); ;
+                .EnsureExistsAsync($"Care package reclaim with id {carePackageReclaimUpdateDomain.Id} not found");
 
-            if (carePackageReclaimUpdateDomain.HasAssessmentBeenCarried)
+            if (!carePackageReclaimUpdateDomain.HasAssessmentBeenCarried)
             {
                 carePackageReclaim.Status = ReclaimStatus.Cancelled;
                 await _dbManager.SaveAsync();
