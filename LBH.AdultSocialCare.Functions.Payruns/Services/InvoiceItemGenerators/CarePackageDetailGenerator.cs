4a2fe8515dba885711a4dd0b082659576875f5d8
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2021-11-30 21:10:48 +0000

    fixed one-off costs generation

--- LBH.AdultSocialCare.Functions.Payruns/Services/InvoiceItemGenerators/CarePackageDetailGenerator.cs
+++ LBH.AdultSocialCare.Functions.Payruns/Services/InvoiceItemGenerators/CarePackageDetailGenerator.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Common.Extensions;
+using LBH.AdultSocialCare.Api.Helpers;
+using LBH.AdultSocialCare.Data.Constants;
 using LBH.AdultSocialCare.Data.Constants.Enums;
 using LBH.AdultSocialCare.Data.Entities.CarePackages;
 using LBH.AdultSocialCare.Data.Entities.Payments;
 using LBH.AdultSocialCare.Functions.Payruns.Domain;
+using LBH.AdultSocialCare.Functions.Payruns.Extensions;
 
 namespace LBH.AdultSocialCare.Functions.Payruns.Services.InvoiceItemGenerators
 {
@@ -14,26 +18,61 @@ namespace LBH.AdultSocialCare.Functions.Payruns.Services.InvoiceItemGenerators
         {
             foreach (var detail in package.Details)
             {
-                var itemRange = GetInvoiceItemDateRange(detail, packageInvoices, invoiceEndDate);
-                if (itemRange.Weeks <= 0) continue;
+                var item = detail.CostPeriod is PaymentPeriod.OneOff
+                    ? CreateOneOffItem(detail, packageInvoices, invoiceEndDate)
+                    : CreateWeeklyItem(detail, packageInvoices, invoiceEndDate);
 
-                yield return new InvoiceItem
-                {
-                    Name = GetItemName(detail),
-                    Quantity = itemRange.Weeks,
-                    WeeklyCost = detail.CostPeriod is PaymentPeriod.OneOff
-                        ? 0.0m
-                        : detail.Cost,
-                    TotalCost = detail.CostPeriod is PaymentPeriod.OneOff
-                        ? detail.Cost
-                        : detail.Cost * itemRange.Weeks,
-                    FromDate = itemRange.StartDate,
-                    ToDate = itemRange.EndDate,
-                    CarePackageDetailId = detail.Id,
-                    SourceVersion = detail.Version,
-                    PriceEffect = PriceEffect.Add
-                };
+                if (item is null) continue;
+
+                yield return item;
             }
+        }
+
+        private static InvoiceItem CreateOneOffItem(CarePackageDetail detail, IList<InvoiceDomain> packageInvoices, DateTimeOffset invoiceEndDate)
+        {
+            // One-off invoice is created just once for entire period, in consequent invoices one-off is ignored
+            var oneOffInvoiceItemExists = packageInvoices.Any(invoice =>
+                invoice.Status.In(InvoiceStatus.Accepted, InvoiceStatus.Held) &&
+                invoice.Items.Any(detail.IsReferenced));
+
+            if (oneOffInvoiceItemExists) return null;
+
+            var startDate = Dates.Max(detail.StartDate, PayrunConstants.DefaultStartDate);
+            var endDate = detail.EndDate ?? startDate.AddDays(1); // arbitrary number just to get quantity 1 in UI for ongoing one-off, review
+
+            if (startDate.UtcDateTime > invoiceEndDate.UtcDateTime) return null;
+
+            return new InvoiceItem
+            {
+                Name = GetItemName(detail),
+                Quantity = Dates.WeeksBetween(startDate, endDate),
+                WeeklyCost = 0.0m,
+                TotalCost = detail.Cost,
+                FromDate = startDate,
+                ToDate = endDate,
+                CarePackageDetailId = detail.Id,
+                SourceVersion = detail.Version,
+                PriceEffect = PriceEffect.Add
+            };
+        }
+
+        private static InvoiceItem CreateWeeklyItem(CarePackageDetail detail, IList<InvoiceDomain> packageInvoices, DateTimeOffset invoiceEndDate)
+        {
+            var itemRange = GetInvoiceItemDateRange(detail, packageInvoices, invoiceEndDate);
+            if (itemRange.Weeks <= 0) return null;
+
+            return new InvoiceItem
+            {
+                Name = GetItemName(detail),
+                Quantity = itemRange.Weeks,
+                WeeklyCost = detail.Cost,
+                TotalCost = detail.Cost * itemRange.Weeks,
+                FromDate = itemRange.StartDate,
+                ToDate = itemRange.EndDate,
+                CarePackageDetailId = detail.Id,
+                SourceVersion = detail.Version,
+                PriceEffect = PriceEffect.Add
+            };
         }
 
         public override IEnumerable<InvoiceItem> CreateRefundItems(CarePackage package, IList<InvoiceDomain> packageInvoices)
@@ -48,10 +87,19 @@ namespace LBH.AdultSocialCare.Functions.Payruns.Services.InvoiceItemGenerators
 
                 foreach (var refund in refunds)
                 {
+                    var quantity = refund.Quantity;
+                    if (detail.CostPeriod is PaymentPeriod.OneOff)
+                    {
+                        var startDate = Dates.Max(detail.StartDate, PayrunConstants.DefaultStartDate);
+                        var endDate = detail.EndDate ?? startDate.AddDays(1); // arbitrary number just to get quantity 1 in UI for ongoing one-off
+
+                        quantity = Dates.WeeksBetween(startDate, endDate);
+                    }
+
                     yield return new InvoiceItem
                     {
                         Name = $"{GetItemName(detail)} (refund)",
-                        Quantity = refund.Quantity,
+                        Quantity = quantity,
                         WeeklyCost = detail.CostPeriod is PaymentPeriod.OneOff
                             ? 0.0m
                             : detail.Cost,
