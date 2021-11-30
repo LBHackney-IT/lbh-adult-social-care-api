0a823c3167bd1d0dc51d2c92e7b1d4ecf451a61e
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2021-11-30 17:25:15 +0000

    updated lambda concurrency settings

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -65,7 +65,6 @@ resource "aws_sqs_queue" "payruns_queue" {
   name                            = "lbh-adult-social-care-payruns-development"
   visibility_timeout_seconds      = 60
   max_message_size                = 2048
-  reserved_concurrent_executions  = 1
 }
 
 resource "aws_sqs_queue_policy" "payruns_queue_to_lambda_policy" {
