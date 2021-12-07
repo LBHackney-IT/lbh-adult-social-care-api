fbf3d01a4738e75791036abca3e26a1f97e69a47
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-12-07 16:23:47 +0000

    fix pay run list filtering

--- LBH.AdultSocialCare.Data/Extensions/GatewayQueryExtensions.cs
+++ LBH.AdultSocialCare.Data/Extensions/GatewayQueryExtensions.cs
@@ -147,8 +147,8 @@ namespace LBH.AdultSocialCare.Data.Extensions
             PayrunType? payrunType, PayrunStatus? payrunStatus, DateTimeOffset? dateFrom,
             DateTimeOffset? dateTo) =>
             payRuns.Where(e => (
-                (searchTerm == null || e.Id.ToString().ToLower().Contains(searchTerm.ToLower()))
-                || (searchTerm == null || e.Number.ToLower().Contains(searchTerm.ToLower()))
+                (searchTerm == null || e.Id.ToString().ToLower().Contains(searchTerm.ToLower())
+                || searchTerm == null || e.Number.ToLower().Contains(searchTerm.ToLower()))
                 && (payrunType == null || e.Type.Equals(payrunType))
                 && (payrunStatus == null || e.Status.Equals(payrunStatus))
                 && (dateFrom == null || e.DateCreated >= dateFrom)
