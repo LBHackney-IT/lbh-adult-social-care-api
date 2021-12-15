3b433a2bf5b8b50738328f22f79456e67114ab1b
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-12-15 11:21:53 +0300

    Update residential release holds val

--- LBH.AdultSocialCare.Data/Constants/Enums/PayrunType.cs
+++ LBH.AdultSocialCare.Data/Constants/Enums/PayrunType.cs
@@ -1,5 +1,5 @@
-using System.ComponentModel;
 using LBH.AdultSocialCare.Data.Attributes;
+using System.ComponentModel;
 
 namespace LBH.AdultSocialCare.Data.Constants.Enums
 {
@@ -9,12 +9,11 @@ namespace LBH.AdultSocialCare.Data.Constants.Enums
         [Description("Residential Recurring")]
         ResidentialRecurring = 1,
 
-        /*[Description("Direct Payments")]
-        DirectPayments = 2,*/
-
         [Description("Residential Released Holds")]
-        ResidentialReleasedHolds = 3,
+        ResidentialReleasedHolds = 2,
 
+        /*[Description("Direct Payments")]
+        DirectPayments = 3,*/
         /*[Description("Direct Payments Released Holds")]
         DirectPaymentsReleasedHolds = 4,*/
     }
