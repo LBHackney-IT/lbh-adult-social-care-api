c8726518edcebdc5f5a96320326c29fcc43c10ed
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2021-12-20 12:25:06 +0300

    corrupt formatting in other way

--- Common/Extensions/GuidExtensions.cs
+++ Common/Extensions/GuidExtensions.cs
@@ -4,7 +4,6 @@ namespace Common.Extensions
 {
     public static class GuidExtensions
     {
-
         public static bool IsEmpty(this Guid? guid)
         {
             return guid is null || guid == Guid.Empty;
