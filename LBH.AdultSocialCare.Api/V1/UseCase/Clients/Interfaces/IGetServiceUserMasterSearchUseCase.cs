2ca90d339dc78f16c0fecf06dfdb2371e1be8ee6
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-12-21 12:33:49 +0000

    add pagination for residents api

--- LBH.AdultSocialCare.Api/V1/UseCase/Clients/Interfaces/IGetServiceUserMasterSearchUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/Clients/Interfaces/IGetServiceUserMasterSearchUseCase.cs
@@ -1,12 +1,12 @@
 using HttpServices.Models.Requests;
 using HttpServices.Models.Responses;
 using System.Threading.Tasks;
+using LBH.AdultSocialCare.Api.V1.Boundary.Common.Response;
 
 namespace LBH.AdultSocialCare.Api.V1.UseCase.Clients.Interfaces
 {
     public interface IGetServiceUserMasterSearchUseCase
     {
-        Task<ServiceUserInformationResponse> GetServiceUsers(ServiceUserQueryRequest request);
-        Task<ServiceUserInformationResponse> GetServiceUsersNew(ServiceUserQueryRequest request);
+        Task<ServiceUserSearchResponse> GetServiceUsers(ServiceUserQueryRequest request);
     }
 }
