1fb0356952b32d53b96b0408a5e82c7e6e963d0a
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2022-01-04 17:56:10 +0000

    Adhoc Payrun should support current date

--- LBH.AdultSocialCare.Api/V1/UseCase/Payments/Concrete/CreateDraftPayRunUseCase.cs
+++ LBH.AdultSocialCare.Api/V1/UseCase/Payments/Concrete/CreateDraftPayRunUseCase.cs
@@ -38,9 +38,13 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
                 throw new ApiException($"Operation not allowed. There exists a pay run that is not approved", HttpStatusCode.PreconditionFailed);
 
             var endOfLastPayRun = await _payRunGateway.GetEndDateOfLastPayRun(draftPayRunCreationDomain.Type);
-            draftPayRunCreationDomain.StartDate = endOfLastPayRun.Date.AddDays(1);
 
-            ValidatePayRunDates(draftPayRunCreationDomain.StartDate, draftPayRunCreationDomain.EndDate);
+            if (draftPayRunCreationDomain.Type == PayrunType.ResidentialRecurring)
+            {
+                draftPayRunCreationDomain.StartDate = endOfLastPayRun.Date.AddDays(1);
+            }
+
+            ValidatePayRunDates(draftPayRunCreationDomain.StartDate, draftPayRunCreationDomain.EndDate, draftPayRunCreationDomain.Type);
 
             draftPayRunCreationDomain.Status = PayrunStatus.Draft;
 
@@ -50,7 +54,7 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
             await _payrunsQueue.Send(payrun.Id);
         }
 
-        private static void ValidatePayRunDates(DateTimeOffset startDate, DateTimeOffset endDate)
+        private static void ValidatePayRunDates(DateTimeOffset startDate, DateTimeOffset endDate, PayrunType payrunType)
         {
             if (startDate > endDate)
             {
@@ -59,7 +63,8 @@ namespace LBH.AdultSocialCare.Api.V1.UseCase.Payments.Concrete
                     HttpStatusCode.UnprocessableEntity);
             }
 
-            if (startDate == endDate)
+            // Start and End Date can't be same if Pay Run Type is Residential Recurring 
+            if (startDate == endDate && payrunType == PayrunType.ResidentialRecurring)
             {
                 throw new ApiException(
                     $"Pay run dates invalid. Pay run cannot start and end on the same day",
