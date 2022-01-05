754fcc33d3eba77fb3bfa9307a1defb06dd1ac44
dunnudge <duncan.okeno@nudgedigital.co.uk>
2022-01-05 18:05:00 +0300

    Pay run list response fix

--- LBH.AdultSocialCare.Api/V1/Boundary/Payments/Response/PayRunListResponse.cs
+++ LBH.AdultSocialCare.Api/V1/Boundary/Payments/Response/PayRunListResponse.cs
@@ -12,8 +12,8 @@ namespace LBH.AdultSocialCare.Api.V1.Boundary.Payments.Response
         public string PayRunStatusName { get; set; }
         public decimal TotalAmountPaid { get; set; }
         public decimal TotalAmountHeld { get; set; }
-        public DateTime DateFrom { get; set; }
-        public DateTime DateTo { get; set; }
+        public DateTimeOffset DateFrom { get; set; }
+        public DateTimeOffset DateTo { get; set; }
         public DateTimeOffset DateCreated { get; set; }
     }
 }
